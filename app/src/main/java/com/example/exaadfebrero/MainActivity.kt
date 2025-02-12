7b9a16b35965b12b0cc354b65b458d53c9fd04c3
Alumno <borjajg02@gmail.com>
2025-02-12 17:57:18 +0100

    Añadido prueba de guardado de 1 cromo

--- app/src/main/java/com/example/exaadfebrero/MainActivity.kt
+++ app/src/main/java/com/example/exaadfebrero/MainActivity.kt
@@ -9,14 +9,18 @@ import com.example.exaadfebrero.features.mushroom.data.MushroomDataRepository
 import com.example.exaadfebrero.features.mushroom.data.local.MushroomXmlLocalDataSource
 import com.example.exaadfebrero.features.mushroom.domain.Album
 import com.example.exaadfebrero.features.mushroom.domain.Mushroom
+import com.example.exaadfebrero.features.mushroom.domain.Sticker
 
 class MainActivity : AppCompatActivity() {
     private lateinit var data: MushroomDataRepository
 
     val albums = listOf(Album("1", "album1", listOf("1", "2", "3")))
+
     val mushroom1 = Mushroom("1", "Boletus", "Boletus", "Seta Comestible")
     val mushroom2 = Mushroom("2", "Amanita", "Amanita", "Seta Venenosa")
     val mushroom3 = Mushroom("3", " Agaricus Campestris.", " Agaricus", "Seta Comestible")
+
+    val sticker = Sticker("1", "1", "2", "image1", "4º", "45º", "12/2/2025", "17:54")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
         data.saveAllAlbums(albums)
         data.saveMushrooms(listOf(mushroom1, mushroom2, mushroom3))
-       // data.getAlbums()
+        data.saveStickers(listOf(sticker))
 
     }
 }
